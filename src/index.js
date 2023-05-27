99147f582b1ab326b5c2563aaf0342a43dd6136d
松本友介 <matsumoto2171205@gmail.com>
2023-05-27 09:50:44 +0900

    5月27日 React学習

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 class Board extends React.Component {
@@ -10,14 +8,33 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X': 'O';
+    this.setState({
+      squares: squares, 
+      xIsNext: !this.state.xIsNext
+    });
   }
   renderSquare(i) {
     return <Square value = {this.state.squares[i]} onClick = {() => this.handleClick(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    };
 
     return (
       <div>
@@ -42,20 +59,31 @@ class Board extends React.Component {
   }
 }
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
+function Square(props) {
+  return (
+    <button className = "square" onClick = {props.onClick}>{props.value}</button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0;i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
-  render() {
-    return (
-      <button class = "square" onClick={() => this.setState({value: 'X'})} >
-        {this.state.value}
-      </button>
-    )
-  }
+  return null;
 }
 
 class Game extends React.Component {
@@ -76,9 +104,3 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-// reportWebVitals();
